e84c98fa4c5574ee70c7b726afd9f7a46be9bc93
iry-lee <28668523+iry-lee@users.noreply.github.com>
2021-10-03 06:58:56 +0000

    add margin to modify the angle loss function

--- layers/__init__.py
+++ layers/__init__.py
@@ -91,9 +91,14 @@ def make_loss_with_center(cfg, num_classes):    # modified by gu
         # 如何在计算中使用角度信息做为约束？
         # 目前只是在全局特征和前景特征中加入了角度的距离约束
         if len(angle_list) > 0:
-            g_angle_loss = angle_loss(cfg, y_global, cls_target, angle_list)
-            f_angle_loss = angle_loss(cfg, y_fore, cls_target, angle_list)
-            # p_angle_loss = angle_loss(cfg, y_part, cls_target, angle_list)
+            if cfg.MODEL.IF_USE_ANGLE_MARGIN:
+                g_angle_loss = margin_angle_loss(cfg, y_global, cls_target, angle_list)
+                f_angle_loss = margin_angle_loss(cfg, y_fore, cls_target, angle_list)
+                # p_angle_loss = margin_angle_loss(cfg, y_part, cls_target, angle_list)
+            else:
+                g_angle_loss = angle_loss(cfg, y_global, cls_target, angle_list)
+                f_angle_loss = angle_loss(cfg, y_fore, cls_target, angle_list)
+                # p_angle_loss = angle_loss(cfg, y_part, cls_target, angle_list)
             return loss + g_angle_loss + f_angle_loss 
         else:
             return loss
@@ -111,6 +116,29 @@ def angle_loss(cfg, feat, cls_target, angle_list):
             if cls_target[i] != cls_target[j] and angle_list[i] == angle_list[j]:
                 distance = F.pairwise_distance(feat[i].unsqueeze(0), feat[j].unsqueeze(0), p=2)
                 diff_id_same_angle_loss = diff_id_same_angle_loss + torch.exp(-1*distance)
+    
+    same_id_diff_angle_loss = cfg.MODEL.SAME_ID_DIFF_VIEW_WEIGHT * same_id_diff_angle_loss
+    diff_id_same_angle_loss = cfg.MODEL.DIFF_ID_SAME_VIEW_WEIGHT * diff_id_same_angle_loss
+
+    loss = 0
+    if cfg.MODEL.SAME_ID_DIFF_VIEW:
+        loss = loss + same_id_diff_angle_loss
+    if cfg.MODEL.DIFF_ID_SAME_VIEW:
+        loss = loss + diff_id_same_angle_loss
+
+    return loss
+
+def margin_angle_loss(cfg, feat, cls_target, angle_list):
+    same_id_diff_angle_loss = 0
+    diff_id_same_angle_loss = 0
+    for i in range(feat.shape[0]):
+        for j in range(i+1, feat.shape[0]):
+            if cls_target[i] == cls_target[j] and angle_list[i] != angle_list[j]:
+                distance = F.pairwise_distance(feat[i].unsqueeze(0), feat[j].unsqueeze(0), p=2)
+                same_id_diff_angle_loss = same_id_diff_angle_loss + max(0, distance - cfg.MODEL.SAME_ID_DIFF_VIEW_MARGIN)
+            if cls_target[i] != cls_target[j] and angle_list[i] == angle_list[j]:
+                distance = F.pairwise_distance(feat[i].unsqueeze(0), feat[j].unsqueeze(0), p=2)
+                diff_id_same_angle_loss = diff_id_same_angle_loss + max(0, cfg.MODEL.DIFF_ID_SAME_VIEW_MARGIN - distance)
     
     same_id_diff_angle_loss = cfg.MODEL.SAME_ID_DIFF_VIEW_WEIGHT * same_id_diff_angle_loss
     diff_id_same_angle_loss = cfg.MODEL.DIFF_ID_SAME_VIEW_WEIGHT * diff_id_same_angle_loss
